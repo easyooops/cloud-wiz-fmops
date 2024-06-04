e187180d8eeda1b3e5158060be70e44870d872c3
SuyeongYoo <ssu0416@gmail.com>
2024-06-04 10:50:00 +0900

    init database model & inquiry

--- src/backend/app/api/router.py
+++ src/backend/app/api/router.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
-from app.api.v1 import provider
+from app.api.v1 import provider, inquiry
 
 api_router = APIRouter()
 
 api_router.include_router(provider.router, tags=["provider"], prefix="/provider")
+api_router.include_router(inquiry.router, tags=["inquiry"], prefix="/inquiry")
