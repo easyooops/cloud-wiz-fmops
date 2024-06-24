74db44a9567943755f762214ed4fdb6f0634334d
dongmin.koo <dongmin.koo@bespinglobal.com>
2024-06-24 16:17:08 +0900

    - embedding 에서 faiss 관련 내용 제거

--- src/backend/app/components/Embedding/OpenAI.py
+++ src/backend/app/components/Embedding/OpenAI.py
@@ -10,11 +10,6 @@ class OpenAIEmbeddingComponent(AbstractEmbeddingComponent):
     def configure(self):
         self.model_instance = OpenAIEmbeddings(api_key=self.openai_api_key)
 
-    # def execute_embed_query(self, input_text):
-    #     if self.model_instance is None:
-    #         raise ValueError("Model instance is not initialized. Call the configure method first.")
-    #     return self.model_instance.embed_query(input_text)
-
     def execute_embed_query(self, input_text: str) -> list[float]:
         if self.model_instance is None:
             raise ValueError("Model instance is not initialized. Call the configure method first.")
@@ -23,11 +18,6 @@ class OpenAIEmbeddingComponent(AbstractEmbeddingComponent):
             return result
         else:
             raise ValueError("Unexpected return type from embed_query")
-
-    # def execute_embed_documents(self, documents: list):
-    #     if self.model_instance is None:
-    #         raise ValueError("Model instance is not initialized. Call the configure method first.")
-    #     return self.model_instance.embed_documents(documents)
 
     def execute_embed_documents(self, documents: list) -> list[list[float]]:
         if self.model_instance is None:
