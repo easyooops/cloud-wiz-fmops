dfdd022b3a579df9ff292ed116493a622ac552d9
SuyeongYoo <ssu0416@gmail.com>
2024-07-12 11:22:08 +0900

    Save current changes before pulling latest updates

--- src/backend/app/service/prompt/service.py
+++ src/backend/app/service/prompt/service.py
@@ -28,7 +28,7 @@ from app.components.LLM.Bedrock import BedrockLLMComponent
 from app.core.util.token import TokenUtilityService
 from app.api.v1.schemas.chat import ChatResponse
 from app.service.store.service import StoreService
-from ddtrace.llmobs.decorators import workflow, task
+from ddtrace.llmobs.decorators import workflow
 from ddtrace.llmobs import LLMObs
 from app.core.util.logging import LoggingConfigurator
 from app.service.processing.model import Processing
@@ -86,12 +86,12 @@ class PromptService:
             # tokens, cost
             tokens = self._get_token_counts(agent_id, query, response)
 
-            # LLMObs.annotate(
-            #     span=None,
-            #     input_data=query,
-            #     output_data=response,
-            #     tags={"host":_d_agent.agent_id, "result": "success"}
-            # )
+            LLMObs.annotate(
+                span=None,
+                input_data=query,
+                output_data=response,
+                tags={"host":_d_agent.agent_id, "result": "success"}
+            )
                 
             return ChatResponse(
                         answer=response,
@@ -100,12 +100,12 @@ class PromptService:
                     )
 
         except Exception as e:
-            # LLMObs.annotate(
-            #     span=None,
-            #     input_data=query,
-            #     output_data=response,
-            #     tags={"host":_d_agent.agent_id, "result": "fail", "error": e}
-            # )            
+            LLMObs.annotate(
+                span=None,
+                input_data=query,
+                output_data=response,
+                tags={"host":_d_agent.agent_id, "result": "fail", "error": e}
+            )            
             raise HTTPException(status_code=500, detail=str(e))
 
     def _get_agent_data(self, agent_id: UUID):
@@ -173,7 +173,7 @@ class PromptService:
 
         _d_agent = agent_data['Agent']
         pre_processing_id = _d_agent.pre_processing_id
-        processing_data = self._get_processing_data(pre_processing_id)
+        processing_data = self._get_processing_data(pre_processing_id)[0]
 
         # pii mask
         if processing_data.pii_masking:
@@ -494,7 +494,7 @@ class PromptService:
 
         _d_agent = agent_data['Agent']
         post_processing_id = _d_agent.post_processing_id
-        processing_data = self._get_processing_data(post_processing_id)
+        processing_data = self._get_processing_data(post_processing_id)[0]
 
         # pii mask
         if processing_data.pii_masking:
--- src/backend/sqlite_dump.sql
+++ src/backend/sqlite_dump.sql
@@ -1,12 +1,12 @@
 -- PRAGMA foreign_keys=OFF;
 START TRANSACTION;
 
-DROP TABLE IF EXISTS alembic_version;
-CREATE TABLE alembic_version (
-	version_num VARCHAR(32) NOT NULL, 
-	CONSTRAINT alembic_version_pkc PRIMARY KEY (version_num)
-);
-INSERT INTO alembic_version VALUES('8bdb50bff9a4');
+-- DROP TABLE IF EXISTS alembic_version;
+-- CREATE TABLE alembic_version (
+-- 	version_num VARCHAR(32) NOT NULL, 
+-- 	CONSTRAINT alembic_version_pkc PRIMARY KEY (version_num)
+-- );
+-- INSERT INTO alembic_version VALUES('8bdb50bff9a4');
 
 DROP TABLE IF EXISTS providers;
 CREATE TABLE providers (
