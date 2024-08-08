0aea06d4e20374f9fb2141792f55d6eb50986860
SuyeongYoo <ssu0416@gmail.com>
2024-08-08 18:02:51 +0900

    Save current changes before pulling latest updates

--- src/frontend/apps/cloud-wiz-management/middleware/router.global.js
+++ src/frontend/apps/cloud-wiz-management/middleware/router.global.js
@@ -5,6 +5,19 @@ export default defineNuxtRouteMiddleware((to, from) => {
 
     const publicPaths = ['/login'];
 
+    // Check if the request path is /google/callback
+    if (!authStore.isAuthenticated && '/google/callback'.includes(to.path)) {
+      // Get the full path including query parameters
+      const fullPath = to.fullPath;
+      const url = import.meta.env.VITE_MANAGEMENT_URL;
+      console.log(fullPath);
+      console.log(url);
+      console.log(`${url}${fullPath}`);
+      // Redirect to the same path on the new domain
+      return navigateTo(`${url}${fullPath}`, { external: true });
+    }
+
+
     if (!authStore.isAuthenticated && !publicPaths.includes(to.path)) {
       return navigateTo('/login');
     }
